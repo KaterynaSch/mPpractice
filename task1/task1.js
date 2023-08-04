4ce56127ebed84d25783621a27eb33188b6a4ee0
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2023-08-04 22:56:02 +0300

    add_task1,3

--- task1/task1.js
+++ task1/task1.js
@@ -1,2 +1,22 @@
 // Створіть просту галерею зображень. У вас є набір мініатюр зображень. Коли користувач клікає на мініатюру, велике зображення має змінитися на відповідне до мініатюри.
 // Додайте клас selected до натиснутого елементу, щоб виділити мініатюру, а також видаліть клас selected з попередньо виділеного елементу.
+
+const box = document.querySelector('#thumbnails');
+const images = box.children;
+const mainImg = document.querySelector('#mainImage');
+
+console.dir(box);
+console.dir(images);
+console.dir(mainImg);
+
+box.addEventListener('click', handlerClick);
+function handlerClick(evt) {
+    console.dir(evt.target);
+    console.dir(evt.currentTarget);
+    if (evt.currentTarget === evt.target) {
+        return;
+    }
+    console.dir(evt.target.src);
+    mainImg.setAttribute('src', evt.target.src);  
+
+}
--- task3/task3.js
+++ task3/task3.js
@@ -1,2 +1,20 @@
 // Задачка:
 // Створіть список елементів, наприклад, <ul id="myList"></ul>. Додайте кнопку, яка буде додавати новий елемент до списку кожного разу, коли на неї натискають. Кожен новий елемент повинен мати унікальний текст, який складається з поточного часу.
+
+const myList = document.querySelector('#myList');
+const myButton = document.querySelector('#myButton');
+
+window.addEventListener('click', handlerClick);
+function handlerClick(evt) {
+
+    if (evt.target !== myButton) {
+        return;
+    }
+    
+    const item = ` <li>
+            <p>${new Date()}</p>
+        </li>`
+    
+    myList.insertAdjacentHTML('beforeend', item);
+    
+};
